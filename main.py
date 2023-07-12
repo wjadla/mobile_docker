6fecfe91a5e1245f5fe8ed2be190c417a75a4af0
wjadla <wjadla@alteca.fr>
2023-07-12 16:19:36 +0200

    "first backend commit mobile application"

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class Person(BaseModel):
+    id: int
+    name: str
+    age: int
+
+DB: List[Person] = [
+    Person(id =1, name ="jamel", age = 22),
+    Person(id =3, name ="alex", age = 18),
+    Person(id =3, name ="ali", age = 33),
+    Person(id =4, name ="mathiew", age = 29)
+]
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World 2023"}
+
+
+@app.get("/api")
+async def read_root():
+    return DB
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
